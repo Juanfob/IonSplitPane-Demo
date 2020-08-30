c0da8477da9ba1554a940c3c28157bb474eebbae
Juan Fermin <juanfob.dev@gmail.com>
2020-08-30 18:30:48 +0200

    Initial upload with the example how to use ion-split-pane

---
+++ src/app/members/home/home-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePage,
+    children: [
+      {
+        path: '', redirectTo: '/home/dashboard', pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('../dashboard/dashboard.module').then( m => m.DashboardPageModule)
+      },
+      {
+        path: 'pagina1',
+        loadChildren: () => import('../pagina1/pagina1.module').then( m => m.Pagina1PageModule)
+      },
+    ]
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomePageRoutingModule {}
